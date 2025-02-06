d578f91c8c2ff3a90dd1418030fd8ab847555e44
shubhamku044 <shubhamku044@gmail.com>
2025-02-06 10:08:56 +0530

    nft project

---
+++ script/DeployMoodNFT.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {MoodNFT} from "../src/MoodNFT.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
+contract DeployMoodNFT is Script {
+    function run() external returns (MoodNFT) {}
+
+    function svgToImageURI(
+        string memory svg
+    ) public pure returns (string memory) {
+        string memory baseURL = "data:image/svg+xml;base64,";
+        string memory svgBase64Encoded = Base64.encode(
+            bytes(string(abi.encodePacked(svg)))
+        );
+
+        return string(abi.encodePacked(baseURL, svgBase64Encoded));
+    }
+}
--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -10,6 +10,8 @@ contract MintBasicNFT is Script {
     string public constant TOKENURI =
         "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
+    address mostRecentlyDeployed = 0xF52369B58d83DF32eef4ebCa0f43ea1f086617aD;
+
     function run() external {
         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
             "BasicNFT",
@@ -18,7 +20,8 @@ contract MintBasicNFT is Script {
 
         mintNFTOnContract(mostRecentlyDeployed);
     }
-    function mintNFTOnContract() public {
+
+    function mintNFTOnContract(address contractAddress) public {
         vm.startBroadcast();
         BasicNFT(contractAddress).mintNFT(TOKENURI);
         vm.stopBroadcast();
